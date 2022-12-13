e155f7071a2b0b389ddf67cb4b882510bc3aa039
Andrej Kiripolský <4126171+AndrejKiri@users.noreply.github.com>
2022-12-13 19:06:58 +0100

    Fix redirect

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+import os
+from flask import Flask,redirect
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return redirect("grafana.com", code=302)
+def hello():
+    return redirect("http://www.grafana.com", code=302)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

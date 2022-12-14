70a41f1491e933d637925ce85511149454058350
Andrej Kiripolský <4126171+AndrejKiri@users.noreply.github.com>
2022-12-14 12:06:23 +0100

    Add counter and printing different strings

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import os
 from flask import Flask,redirect
+from multiprocessing import Value
+
+counter = Value('i', 0)
+## a = "http://www.grafana.com"
+## a = 1
+## a = a+1
+
+stacks = [
+    "NA",
+    "http://www.grafana.com",
+    "http://www.google.com"
+]
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return redirect("http://www.grafana.com", code=302)
+    with counter.get_lock():
+            counter.value += 1
+            out = counter.value
+    return f'{stacks[out]}'
+##    return redirect(a, code=302)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.

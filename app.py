58d7f04bc302c05c08f0a16f47a06f9afd7514ca
Andrej Kiripolský <4126171+AndrejKiri@users.noreply.github.com>
2023-03-29 19:05:20 +0200

    Add reading from file and url construction

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ import random
 
 counter = Value('i', 0)
 
-stacks = [
-    "https://s38p1.grafana.net/",
-    "https://s38p2.grafana.net/",
-    "https://s38p3.grafana.net/",
-    "https://s38p4.grafana.net/",
-    "https://s38p5.grafana.net/",
-    "https://s38p6.grafana.net/",
-    "https://s38p7.grafana.net/",
-    "https://s38p8.grafana.net/",
-    "https://s38p9.grafana.net/",
-    "https://s38p10.grafana.net/"
-]
+# stacks = [
+#     "https://s38p1.grafana.net/",
+#     "https://s38p2.grafana.net/",
+#     "https://s38p3.grafana.net/",
+#     "https://s38p4.grafana.net/",
+#     "https://s38p5.grafana.net/",
+#     "https://s38p6.grafana.net/",
+#     "https://s38p7.grafana.net/",
+#     "https://s38p8.grafana.net/",
+#     "https://s38p9.grafana.net/",
+#     "https://s38p10.grafana.net/"
+# ]
+
+file = open("test-stacks.txt", "r")
+
+with open('test-stacks.txt') as f:
+    stacks = f.read().splitlines()
 
 app = Flask(__name__)
 
@@ -25,10 +30,11 @@ def hello():
     # out = random.randrange(9)
     # return redirect(stacks[out], code=302)
     with counter.get_lock():
+            url = 'https://' + stacks[counter.value] + '.grafana.net'
             out = counter.value
             counter.value += 1
     # return f'{stacks[out]}'
-    return redirect(stacks[out], code=302) 
+    return redirect(url, code=302) 
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.

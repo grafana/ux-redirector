c77b6eb8181bd8d459b750bfaff5a8b5c2a14518
Andrej Kiripolský <4126171+AndrejKiri@users.noreply.github.com>
2023-04-15 00:40:15 +0200

    Make random redirects work

--- main.py
+++ main.py
@@ -14,15 +14,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # out = random.randrange(len(stacks))
-    # return redirect(stacks[out], code=302)
-    with counter.get_lock():
-            url = f'https://{stacks[counter.value]}.grafana.net'
-            out = counter.value
-            counter.value += 1
-    return redirect(url, code=302) 
+    out = random.randrange(len(stacks))
+    url = f'https://{stacks[out]}.grafana.net'
+    return redirect(url, code=302)
+    # with counter.get_lock():
+    #         url = f'https://{stacks[counter.value]}.grafana.net'
+    #         out = counter.value
+    #         counter.value += 1
+    # return redirect(url, code=302) 
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+# Reminder: gunicorn -b:3030 main:app
